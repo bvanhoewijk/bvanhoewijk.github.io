9f801eefaa4382e283e5a767e4940bc9c718776d
Bas van Hoewijk <basvanhoewijk@gmail.com>
2016-11-10 20:40:00 +0100

    cleanup

--- script/parsegoodreads.py
+++ script/parsegoodreads.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import xml.etree.ElementTree as ET
+import sys
 from HTMLParser import HTMLParser
 
-
-root = ET.parse('read.xml').getroot()
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -30,38 +29,42 @@ def getStars(rating):
         stars = "N/A"
     else:
         for i in range(0, int(rating)):
-            stars += "<span class=\"glyphicon glyphicon-star\" aria-hidden=\"true\"></span>"
+            stars += '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>'
     return stars
 
-print '<thead><tr><th /><th>Title</th><th>Author</th><th>Rating</th></tr></thead><tbody>'
 
-bookindex = 0
-for item in root.iter('item'):
-    bookindex = bookindex + 1
-    title = cleantext(item.find('title').text)
-    author = cleantext(item.find('author_name').text)
-    rating = cleantext(item.find('user_rating').text)
-    img = cleantext(item.find('book_small_image_url').text)
-    user_review = cleantext(item.find('user_review').text)
-    book_description = cleantext(item.find('book_description').text)
+def main():
+    goodreadsxml = sys.argv[1]
 
-    book_description = strip_tags(book_description)
-    if user_review != None: 
-        user_review = strip_tags(user_review)
-    else:
-        user_review = str(rating) + " stars"
+    root = ET.parse(goodreadsxml).getroot()
+    print '<thead><tr><th /><th>Title</th><th>Author</th><th>My rating</th></tr></thead><tbody>'
 
-    print '<tr class="accordion-toggle" data-toggle="collapse" data-target="#%s">' % ("book" + str(bookindex))
-    print "<td><img src=\"%s\"/></td>" % img
-    print "<td>%s</td>" % title
-    print "</td>"
-    print "<td>%s</td>" % author
-    print "<td><a href=\"#\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"%s\">" % user_review
+    bookindex = 0
+    for item in root.iter('item'):
+        bookindex = bookindex + 1
+        title = cleantext(item.find('title').text)
+        author = cleantext(item.find('author_name').text)
+        rating = cleantext(item.find('user_rating').text)
+        img = cleantext(item.find('book_small_image_url').text)
+        user_review = cleantext(item.find('user_review').text)
+        book_description = cleantext(item.find('book_description').text)
 
-    print getStars(rating)
+        book_description = strip_tags(book_description)
+        if user_review != None: 
+            user_review = strip_tags(user_review)
+        else:
+            user_review = str(rating) + " stars"
 
-    print "</td></tr>"
-    print '<tr><td class="zeroPadding" colspan="4"><div id="%s" class="collapse">%s</div></tr>' % ("book" + str(bookindex), book_description)    
+        print '<tr class="accordion-toggle" data-toggle="collapse" data-target="#%s">' % ("book" + str(bookindex))
+        print '<td><img src="%s"/></td>' % img
+        print '<td>%s</td>' % title
+        print '<td>%s</td>' % author
+        print '<td><a href="#" data-toggle="tooltip" data-placement="top" title="%s">' % user_review
+        print getStars(rating)
+        print '</td></tr>'
+        print '<tr><td class="zeroPadding" colspan="4"><div id="%s" class="collapse">%s</div></tr>' % ("book" + str(bookindex), book_description)    
 
-print '</tbody>'
+    print '</tbody>'
 
+if __name__ == "__main__":
+    main()
